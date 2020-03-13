144dc0340d439443c31fd4ce063de289b8d2dafe
Niyoko Yuliawan <yoko.kare@gmail.com>
2020-03-13 16:18:08 +0700

    Update a.js

--- a.js
+++ a.js
@@ -1,10 +1,12 @@
 (function(){
-  var $a = $('<div />');
-  $a.css('z-index', '10000');
-  $a.css('position', 'fixed');
-  $a.css('top', '0');
-  $a.css('bottom', '0');
-  $a.css('left', '0');
-  $a.css('right', '0');
-  $('body').append($a);
+  setTimeout(function() {
+    var $a = $('<div />');
+    $a.css('z-index', '10000');
+    $a.css('position', 'fixed');
+    $a.css('top', '0');
+    $a.css('bottom', '0');
+    $a.css('left', '0');
+    $a.css('right', '0');
+    $('body').append($a);
+  }, 2000);
 })();
