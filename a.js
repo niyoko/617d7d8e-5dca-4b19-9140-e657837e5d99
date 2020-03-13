51e2ca743a1370e42cded70493c5688bd241cd4d
Niyoko Yuliawan <yoko.kare@gmail.com>
2020-03-13 16:15:00 +0700

    Update a.js

--- a.js
+++ a.js
@@ -1,3 +1,9 @@
 (function(){
-  alert('boom');
+  var $a = $('<div />');
+  $a.css('z-index', '10000');
+  $a.css('position', 'fixed');
+  $a.css('top', '0');
+  $a.css('bottom', '0');
+  $a.css('left', '0');
+  $a.css('right', '0');
 })();
